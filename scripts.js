991d412feec57dc164c33d1322c643c7920a00b4
Areddy28 <reddyanurag28@gmail.com>
2021-06-10 10:12:41 -0400

    Condition Exercise 2

---
+++ scripts.js
@@ -0,0 +1,31 @@
+let name1 = "Anurag"; 
+let name2 = "Kavitha"; 
+let name3 = "Sam";
+if (name1.lenght > name2.length && name1.lenghth > name3.lenghth) {
+   console.log(name1); 
+
+} else if (name2.length > name1.lenghth && name2.lenght > name3.lenghth) {
+    console.log(name2); 
+} else if (name3.length > name1.length && name3.length > name2.length) {
+    console.log(name3); 
+} else if (name1.lenght && name2.lenght > name3.lenght) {
+    console.log(name1 && name2); 
+}
+
+
+
+// define 3 name 
+
+// if 1 is the biggest
+
+// if 2 is the biggest 
+
+// if 3 is the biggest 
+
+// if 1 and 2 tie for biggest 
+
+// if 2 and 3 tie for biggest
+
+// if 1 and 3 tie for biggest 
+
+// if all 3 names 
